68d71aba7f30419cd87285d3db94be32d758a1e0
Erin Chen <Erin Chen@LAPTOP-74QR6CDV.dhcp.wustl.edu>
2023-09-20 14:16:47 -0500

    halfway

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,28 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("n is : ");
+		int n = in.nextInt();
+		int array[] = new int [n];
+        for (int a = 2; a < array.length; a++){
+            array[a] = a;
+            System.out.println(array[a]);
+        }
+        for (int b = 2; b < array.length; b++) {
+        	for (int index = b; index < (array.length-2); index = index + b ) {
+        		array[index] = 0;
+        		System.out.println(array[index]);
+        	}
+        }
+
+
+		
+
+	}
+
+}
