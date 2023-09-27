07f15e649a53b4dcadfadadf0e69a843b9eb7223
Erin Chen <Erin Chen@LAPTOP-74QR6CDV.dhcp.wustl.edu>
2023-09-26 22:23:51 -0500

    sort of works

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -8,16 +8,19 @@ public class Sieve {
 		Scanner in = new Scanner(System.in);
 		System.out.println("n is : ");
 		int n = in.nextInt();
-		int array[] = new int [n];
+		int array[] = new int [n+1];
         for (int a = 2; a < array.length; a++){
             array[a] = a;
             System.out.println(array[a]);
         }
         for (int b = 2; b < array.length; b++) {
-        	for (int index = b; index < (array.length-2); index = index + b ) {
+        	for (int index = b+b; index < (array.length-1); index = index + b ) {
         		array[index] = 0;
-
         	}
+        	for (int i = 0; i < array.length; i ++) {
+        		System.out.print(array[i]+" ");
+        	}
+        	System.out.println();
         }
 
 
